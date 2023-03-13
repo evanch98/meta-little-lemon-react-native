e8276a758af1e698c25331a0c485fc9a11633a1f
Kyaw Thu <evanch98@gmail.com>
2023-03-13 20:17:10 +0800

    Add Image and Text

--- screens/SubscribeScreen.js
+++ screens/SubscribeScreen.js
@@ -1,9 +1,33 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { View, Image, StyleSheet, Text } from 'react-native';
 
 const SubscribeScreen = () => {
   // Add subscribe screen code here
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require('../assets/little-lemon-logo-grey.png')} />
+      <Text style={styles.bodyText}>Subscribe to our newsletter for our latest delicious recipes</Text>
+    </View>
+  );
 };
 
 export default SubscribeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 30
+  },
+  logo: {
+    height: 150,
+    width: 150,
+    resizeMode: 'contain',
+    marginTop: 50
+  },
+  bodyText: {
+    fontSize: 20,
+    marginTop: 50,
+    textAlign: 'center'
+  }
+});
