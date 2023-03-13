518875c25f77c81974839049f8a414f1af56d7ed
Kyaw Thu <evanch98@gmail.com>
2023-03-13 20:40:35 +0800

    Add button:

--- screens/SubscribeScreen.js
+++ screens/SubscribeScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Image, StyleSheet, Text, TextInput } from 'react-native';
+import { View, Image, StyleSheet, Text, TextInput, Pressable } from 'react-native';
 
 const SubscribeScreen = () => {
   // Add subscribe screen code here
@@ -15,6 +15,11 @@ const SubscribeScreen = () => {
         onChangeText={onChangeEmail}
         placeholder='example@email.com'
       />
+      <Pressable 
+        style={styles.button}
+        onPress={() => null}>
+        <Text style={styles.buttonText}>Subscribe</Text>
+      </Pressable>
     </View>
   );
 };
@@ -46,5 +51,19 @@ const styles = StyleSheet.create({
     fontSize: 16,
     borderColor: '#333333',
     borderRadius: 10
-  }
+  },
+  button: {
+    fontSize: 22,
+    padding: 5,
+    marginVertical: 20,
+    backgroundColor: '#007300',
+    borderColor: '#007300',
+    borderWidth: 2,
+    borderRadius: 10
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 20,
+  },
 });
