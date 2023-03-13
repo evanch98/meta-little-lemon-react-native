bf88b7eb392cf35d25c381be31946d9c17a7e77a
Kyaw Thu <evanch98@gmail.com>
2023-03-13 20:31:13 +0800

    Add TextInput

--- screens/SubscribeScreen.js
+++ screens/SubscribeScreen.js
@@ -1,12 +1,20 @@
 import * as React from 'react';
-import { View, Image, StyleSheet, Text } from 'react-native';
+import { View, Image, StyleSheet, Text, TextInput } from 'react-native';
 
 const SubscribeScreen = () => {
   // Add subscribe screen code here
+  const [email, onChangeEmail] = React.useState('');
+
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require('../assets/little-lemon-logo-grey.png')} />
       <Text style={styles.bodyText}>Subscribe to our newsletter for our latest delicious recipes</Text>
+      <TextInput
+        style={styles.emailInput} 
+        value={email}
+        onChangeText={onChangeEmail}
+        placeholder='example@email.com'
+      />
     </View>
   );
 };
@@ -16,18 +24,27 @@ export default SubscribeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     padding: 30
   },
   logo: {
     height: 150,
     width: 150,
     resizeMode: 'contain',
-    marginTop: 50
+    marginTop: 50,
+    alignSelf: 'center'
   },
   bodyText: {
     fontSize: 20,
     marginTop: 50,
     textAlign: 'center'
+  },
+  emailInput: {
+    height: 40,
+    marginTop: 30,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 16,
+    borderColor: '#333333',
+    borderRadius: 10
   }
 });
