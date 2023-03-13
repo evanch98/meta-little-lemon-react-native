12361ab14a7c25e3379649b3142f6c9303dc36b2
Kyaw Thu <evanch98@gmail.com>
2023-03-13 19:35:26 +0800

    Add Text and align contents

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, Text } from 'react-native';
 
 const WelcomeScreen = ({ navigation }) => {
   // Add welcome screen code here.
   return (
   <View style={styles.container}>
     <Image style={styles.logo} source={require('../assets/little-lemon-logo.png')} />
+    <Text style={styles.bodyText}>Little Lemon, your local Mediterranean Bistro</Text>
   </View>
   );
 };
@@ -14,11 +15,16 @@ export default WelcomeScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   logo: {
     height: 250,
     width: 350,
-    resizeMode: 'contain'
+    resizeMode: 'contain',
+  },
+  bodyText: {
+    fontSize: 30
   }
 });
