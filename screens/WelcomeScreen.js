55021c6135898fd883656c8536bbeba9f7d3e0cc
Kyaw Thu <evanch98@gmail.com>
2023-03-13 19:24:05 +0800

    Add Image on the WelcomeScreen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,9 +1,21 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 
 const WelcomeScreen = ({ navigation }) => {
   // Add welcome screen code here.
-  return <View></View>;
+  return (
+  <View>
+    <Image style={styles.logo} source={require('../assets/little-lemon-logo.png')} />
+  </View>
+  );
 };
 
 export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  logo: {
+    height: 300,
+    width: 400,
+    resizeMode: 'contain'
+  }
+});
